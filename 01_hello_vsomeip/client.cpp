a65605362ec544aaac24527f2e84f57b054d05a5
vu3.nguyen <vu3.nguyen@lge.com>
2025-02-13 11:54:18 +0900

    hello vsomeip app

--- 01_hello_vsomeip/client.cpp
+++ 01_hello_vsomeip/client.cpp
@@ -1,7 +1,13 @@
 #include <vsomeip/vsomeip.hpp>
+#include <memory>
+#include <iostream>
 
+std::shared_ptr<vsomeip::application> app;
 
 int main()
 {
+    app = vsomeip::runtime::get()->create_application("hello_vsomeip_client");
+    app->init();
+    app->start();
     return 0;
 }
--- 01_hello_vsomeip/service.cpp
+++ 01_hello_vsomeip/service.cpp
@@ -1,6 +1,13 @@
 #include <vsomeip/vsomeip.hpp>
+#include <memory>
+#include <iostream>
+
+std::shared_ptr<vsomeip::application> app;
 
 int main()
 {
+    app = vsomeip::runtime::get()->create_application("hello_vsomeip_service");
+    app->init();
+    app->start();
     return 0;
 }
